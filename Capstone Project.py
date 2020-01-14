cb7fe68d5417af55db8b18eb08a2331f75947470
nikimore98 <59871645+nikimore98@users.noreply.github.com>
2020-01-14 17:24:23 +0530

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is mainly used for Captsone Project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Captsone Project Course!")
+
